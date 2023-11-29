2b17ce314e9c88d7439b2a1b398ae9146261e8e4
Moahmed Bahaa <mohamedelgohre956@gmail.com>
2023-11-29 23:06:35 +0200

    Done

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(-10 , 10)
+if number > 0:
+    print(f" The numbder is {number},so It is positive.")
+elif number == 0:
+  print(f"The numbder is {number}, so it is zero.")
+else:
+   print(f"The numbder is {number}, so it is negtize.")  
