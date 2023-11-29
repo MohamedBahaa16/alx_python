7aa0355d0b0ea5856aca608fd847d98300166289
Moahmed Bahaa <mohamedelgohre956@gmail.com>
2023-11-29 22:32:27 +0200

    Okay

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,5 +1,7 @@
 print("""
       [Got]
+
+      [Got]
       Programming is like building a multilingual puzzle
       (51 chars long)
       [stderr]: 
@@ -9,4 +11,15 @@ print("""
       (52 chars long)
       [stderr]: [Anything]
       (0 chars long)
+      
+
+(282 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+"Programming is like building a multilingual puzzle
+
+(52 chars long)
+[stderr]: [Anything]
+(0 chars long)
       """)
