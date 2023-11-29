ae00e0f92ba55dfec81cb1ab2d1fdc6b3f71d1fe
Moahmed Bahaa <mohamedelgohre956@gmail.com>
2023-11-29 22:51:54 +0200

    Done

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,7 @@
+word = "Holberton"
+#contain the first 3 letters of the variable word
+print(f"{word[:3]}")
+#contain the last 2 letters of the variable word
+print(f"{word[-2:]}")
+#contain the value of the variable word without the first and last letters
+print(f"{word[1:8]}")
