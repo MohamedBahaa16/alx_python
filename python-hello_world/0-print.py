ace2b36b5bc8b24d5310962b5303ae83035dbff2
Moahmed Bahaa <mohamedelgohre956@gmail.com>
2023-11-29 22:58:51 +0200

    Done

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,16 @@
-print("guillaume@ubuntu:~/py/$ python3 0-print.py")
-print("Programming is like building a multilingual puzzle")
-print("guillaume@ubuntu:~/py/$")
+print("""
+[Got]
+guillaume@ubuntu:~/py/$ python3 0-print.py
+Programming is like building a multilingual puzzle
+guillaume@ubuntu:~/py/$
+
+(118 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+"Programming is like building a multilingual puzzle
+
+(52 chars long)
+[stderr]: [Anything]
+(0 chars long)      
+      """)
--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-print("guillaume@ubuntu:~/py/$ python3 1-edges.py")
+print("Hello")
 word = "Holberton"
 #contain the first 3 letters of the variable word
 print(f"First 3 letters: {word[:3]}")
@@ -6,8 +6,3 @@ print(f"First 3 letters: {word[:3]}")
 print(f"Last 2 letters: {word[-2:]}")
 #contain the value of the variable word without the first and last letters
 print(f"Middle word: {word[1:8]}")
-print("""
-guillaume@ubuntu:~/py/$ wc -l 1-edges.py
-8 1-edges.py
-guillaume@ubuntu:~/py/$ 
-      """)
