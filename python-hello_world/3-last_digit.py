a374922f58b83f12b661133bea9a677aa5fbf29d
Moahmed Bahaa <mohamedelgohre956@gmail.com>
2023-11-30 19:22:24 +0200

    Done

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,1 +1,11 @@
 import random
+number = str(random.randint(-1000,1000))
+if int(number[-1:]) > 5: 
+   print(f"It is {number[-1:]} and is greater than 5 ")
+elif int(number[-1:]) == 0:
+   print(f"It is{number[-1:]} and is zero")
+else:
+    print(f"The {number[-1:]} is less than 6 and is less than 6 and not 0")
+
+
+
